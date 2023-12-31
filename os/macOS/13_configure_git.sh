dd80f76b5cf28fefc22c3e36516bf2a57ba42c57
Tony Duran <tony-duran@outlook.com>
2023-12-30 21:38:33 -0600

    adding git zsh link scripts

---
+++ os/macOS/13_configure_git.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Source common functions
+source ../../common/common_functions.sh
+
+# Path to your dotfiles repository
+DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd ../.. && pwd)"
+
+# Path to the git configuration in your dotfiles repository
+GIT_CONFIG_SRC="${DOTFILES_DIR}/config/git/gitconfig"
+
+# Path where the git config should be linked in the home directory
+GIT_CONFIG_DEST="${HOME}/.gitconfig"
+
+# Directory for backup files
+BACKUP_DIR="${HOME}/backup_files"
+
+echo "Configuring Git..."
+
+# Check if a .gitconfig file already exists and is not a symlink
+if [ -f "${GIT_CONFIG_DEST}" ] && [ ! -L "${GIT_CONFIG_DEST}" ]; then
+    echo "Existing .gitconfig found. Backing up..."
+    mkdir -p "${BACKUP_DIR}"
+    mv "${GIT_CONFIG_DEST}" "${BACKUP_DIR}/gitconfig.bk"
+fi
+
+# Remove existing symlink to avoid conflicts
+if [ -L "${GIT_CONFIG_DEST}" ]; then
+    echo "Removing existing symlink at ${GIT_CONFIG_DEST}"
+    rm "${GIT_CONFIG_DEST}"
+fi
+
+# Create a symlink to gitconfig in the home directory
+echo "Creating symlink for git config"
+ln -s "${GIT_CONFIG_SRC}" "${GIT_CONFIG_DEST}"
+echo "Git configuration is set up."
+
---
+++ os/macOS/14_configure_zsh.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Source common functions
+source ../../common/common_functions.sh
+
+# Path to your dotfiles repository
+DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd ../.. && pwd)"
+
+# Path to the zsh configuration in your dotfiles repository
+ZSH_CONFIG_SRC="${DOTFILES_DIR}/config/zsh/zshrc"
+
+# Path where the zsh config should be linked in the home directory
+ZSH_CONFIG_DEST="${HOME}/.zshrc"
+
+# Directory for backup files
+BACKUP_DIR="${HOME}/backup_files"
+
+echo "Configuring Zsh..."
+
+# Check if a .zshrc file already exists and is not a symlink
+if [ -f "${ZSH_CONFIG_DEST}" ] && [ ! -L "${ZSH_CONFIG_DEST}" ]; then
+    echo "Existing .zshrc found. Backing up..."
+    mkdir -p "${BACKUP_DIR}"
+    mv "${ZSH_CONFIG_DEST}" "${BACKUP_DIR}/zshrc.bk"
+fi
+
+# Remove existing symlink to avoid conflicts
+if [ -L "${ZSH_CONFIG_DEST}" ]; then
+    echo "Removing existing symlink at ${ZSH_CONFIG_DEST}"
+    rm "${ZSH_CONFIG_DEST}"
+fi
+
+# Create a symlink to zshrc in the home directory
+echo "Creating symlink for zsh config"
+ln -s "${ZSH_CONFIG_SRC}" "${ZSH_CONFIG_DEST}"
+echo "Zsh configuration is set up."
+
