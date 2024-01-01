ba7ca7de7b4149662116fddb5bbf4e7264526e96
Tony Duran <tony-duran@outlook.com>
2024-01-01 16:24:43 -0600

    fixing zsh script to avoid pause

--- os/shared/01_install_zsh.sh
+++ os/shared/01_install_zsh.sh
@@ -10,7 +10,10 @@ if [ ! -d "${OH_MY_ZSH_DIR}" ]; then
     echo "Oh My Zsh is not installed. Installing now..."
 
     # Install Oh My Zsh
-    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    #sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" CHSH=no RUNZSH=no
+    echo "y" | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
 
     # Check if the installation was successful
     if [ ! -d "${OH_MY_ZSH_DIR}" ]; then
