f2ce85bd07c8569c7b17d999768ccced934f8085
Tony Duran <tony-duran@outlook.com>
2024-01-01 10:54:14 -0600

    adding new helper function

--- common/common_functions.sh
+++ common/common_functions.sh
@@ -5,3 +5,15 @@ command_exists() {
     command -v "$1" &> /dev/null
 }
 
+find_repo_root() {
+    local dir=$1
+    while [[ $dir != / ]]; do
+        if [[ -d "$dir/.git" ]]; then
+            echo "$dir"
+            return
+        fi
+        dir=$(dirname "$dir")
+    done
+    echo "Error: Unable to find the root of the repository." >&2
+    exit 1
+}
