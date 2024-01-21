8a1f28e9b125a514349093e8ef0c994d7b9384bd
Tony Duran <tony-duran@outlook.com>
2024-01-20 21:33:57 -0600

    modification to tmux to get linux-specific fixes

--- os/shared/15_configure_tmux.sh
+++ os/shared/15_configure_tmux.sh
@@ -5,16 +5,25 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "$DIR/../../etc/helpers.sh"
 
 # Path to your dotfiles repository
-pushd $(find_repo_root "$(dirname "${BASH_SOURCE[0]}")")
+pushd "$(find_repo_root "$(dirname "${BASH_SOURCE[0]}")")"
 
 # Path to the tmux configuration in your dotfiles repository
 TMUX_CONFIG_SRC="${PWD}/config/tmux/tmux.conf"
+TMUX_MACOS_CONFIG_SRC="${PWD}/config/tmux/tmux-macosx.conf"
+TMUX_LINUX_CONFIG_SRC="${PWD}/config/tmux/tmux-linux.conf"
 
 # Path where the tmux config should be linked in the home directory
 TMUX_CONFIG_DEST="${HOME}/.tmux.conf"
+TMUX_CONFIG_DIR="${HOME}/.config/tmux"
 
 # Directory for backup files
 BACKUP_DIR="${HOME}/backup_files"
+
+# Create the ~/.config/tmux directory if it doesn't exist
+if [ ! -d "${TMUX_CONFIG_DIR}" ]; then
+    echo "Creating ${TMUX_CONFIG_DIR} directory..."
+    mkdir -p "${TMUX_CONFIG_DIR}"
+fi
 
 # Check if tmux is installed
 if command_exists tmux; then
@@ -38,8 +47,14 @@ if command_exists tmux; then
     ln -s "${TMUX_CONFIG_SRC}" "${TMUX_CONFIG_DEST}"
     echo "tmux configuration is set up."
 
+    # Symlink macOS and Linux specific tmux configurations
+    ln -sf "${TMUX_MACOS_CONFIG_SRC}" "${TMUX_CONFIG_DIR}/tmux-macosx.conf"
+    ln -sf "${TMUX_LINUX_CONFIG_SRC}" "${TMUX_CONFIG_DIR}/tmux-linux.conf"
+    echo "tmux platform-specific configurations are set up."
+
 else
     echo "tmux is not installed. Please install tmux first."
 fi
 
 popd
+
